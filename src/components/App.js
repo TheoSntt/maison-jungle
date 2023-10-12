720e98b4d2ee906165a5a86db97bbdeeb27f6ccc
Théo <sinatti.theo@gmail.com>
2023-10-12 10:59:41 +0200

    Use useEffect to store user cart in localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,16 @@ import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import Footer from './Footer'
 import '../styles/Layout.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 function App() {
-	const [cart, updateCart] = useState([])
+	const savedCart = localStorage.getItem('cart')
+	// const [cart, updateCart] = useState([])
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
 
 	return (
 		<div>
